3a49e70e177f5b8fd9b49edfefb3bf97d7440e78
josef <osobni@josefbednar.com>
2026-02-16 22:36:21 +0100

    match saving

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
 from os import listdir
+import os
 from PIL import Image
 import torch.nn as nn
 import torch
+
+from datasets import load_dataset
 
 from clip_model import CLIPModel
 from my_cosplace_model import CosPlaceModel
@@ -20,6 +23,30 @@ from database import Database
 @dataclass
 class config:
     device: str = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+def save_matches(ids):
+    print(f"Streaming dataset to retrieve {len(ids)} images...")
+    
+    # Authenticated streaming load
+    ds = load_dataset("josefbednar/prague-streetview-50k", split="train", streaming=True)
+    
+    os.makedirs("matches", exist_ok=True)
+    found = 0
+
+    for row in ds:
+        if row['image_id'] in ids:
+            # Hugging Face downloads the image bytes only here
+            img = row['image']
+            save_path = f"matches/{row['image_id']}.jpg"
+            img.save(save_path)
+            
+            print(f"Saved: {save_path}")
+            found += 1
+            
+            if found >= len(ids):
+                print("All matches found!")
+                break
 
 
 def main():
@@ -44,6 +71,9 @@ def main():
     print(f'Initial rankings:')
     for idx, result in enumerate(results):
         print(f'{idx + 1}.\t{result['filename']}\t{result['_distance']}')
+
+    ids = [r['filename'] for r in results]
+    save_matches(ids)
 
     # print(f'Refined rankings:')
     # for idx, item in enumerate(ranked):
